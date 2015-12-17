4c191430b5428282d650b31ed64891f84e1cab32
Evgenii-v <enejmail@gmail.com>
2015-12-17 13:41:08 +0200

    Create webinarorder.php

---
+++ webinarorder.php
@@ -0,0 +1,100 @@
+<?php
+class ControllerAccountWebinarorder extends Controller {
+	private $error = array();
+	
+		public function info() {
+		$this->load->language('account/order');
+
+		if (isset($this->request->get['order_id'])) {
+			$order_id = $this->request->get['order_id'];
+		} else {
+			$order_id = 0;
+		}
+
+		if (!$this->customer->isLogged()) {
+			$this->session->data['redirect'] = $this->url->link('account/webinar/info', 'order_id=' . $order_id, 'SSL');
+
+			$this->response->redirect($this->url->link('account/login', '', 'SSL'));
+		}
+
+		$this->load->model('account/order');
+
+		$order_info = $this->model_account_order->getOrder($order_id);
+
+		if ($order_info) {
+			$this->document->setTitle($this->language->get('text_order'));
+
+			$data['breadcrumbs'] = array();
+
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('text_home'),
+				'href' => $this->url->link('common/home')
+			);
+
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('text_account'),
+				'href' => $this->url->link('account/account', '', 'SSL')
+			);
+
+			$url = '';
+
+			if (isset($this->request->get['page'])) {
+				$url .= '&page=' . $this->request->get['page'];
+			}
+
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('heading_title'),
+				'href' => $this->url->link('account/webinar', $url, 'SSL')
+			);
+
+			$data['breadcrumbs'][] = array(
+				'text' => $this->language->get('text_order'),
+				'href' => $this->url->link('account/webinar/info', 'order_id=' . $this->request->get['order_id'] . $url, 'SSL')
+			);
+
+			$data['heading_title'] = $this->language->get('text_order');
+
+			$data['text_order_detail'] = $this->language->get('text_order_detail');
+			$data['text_invoice_no'] = $this->language->get('text_invoice_no');
+			$data['text_order_id'] = $this->language->get('text_order_id');
+			$data['text_date_added'] = $this->language->get('text_date_added');
+			$data['text_shipping_method'] = $this->language->get('text_shipping_method');
+			$data['text_shipping_address'] = $this->language->get('text_shipping_address');
+			$data['text_payment_method'] = $this->language->get('text_payment_method');
+			$data['text_payment_address'] = $this->language->get('text_payment_address');
+			$data['text_history'] = $this->language->get('text_history');
+			$data['text_comment'] = $this->language->get('text_comment');
+
+			$data['column_name'] = $this->language->get('column_name');
+			$data['column_model'] = $this->language->get('column_model');
+			$data['column_quantity'] = $this->language->get('column_quantity');
+			$data['column_price'] = $this->language->get('column_price');
+			$data['column_total'] = $this->language->get('column_total');
+			$data['column_action'] = $this->language->get('column_action');
+			$data['column_date_added'] = $this->language->get('column_date_added');
+			$data['column_status'] = $this->language->get('column_status');
+			$data['column_comment'] = $this->language->get('column_comment');
+
+			$data['button_reorder'] = $this->language->get('button_reorder');
+			$data['button_return'] = $this->language->get('button_return');
+			$data['button_continue'] = $this->language->get('button_continue');
+
+			if (isset($this->session->data['error'])) {
+				$data['error_warning'] = $this->session->data['error'];
+
+				unset($this->session->data['error']);
+			} else {
+				$data['error_warning'] = '';
+			}
+
+			if (isset($this->session->data['success'])) {
+				$data['success'] = $this->session->data['success'];
+
+				unset($this->session->data['success']);
+			} else {
+				$data['success'] = '';
+			}
+			
+			
+   }
+  ] 
